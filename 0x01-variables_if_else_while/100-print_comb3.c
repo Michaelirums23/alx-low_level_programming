8b3ff4079e458d770332592580bc868117cd687d
Michael <oshomike35@gmail.com>
2023-08-14 18:25:30 -0700

    Print combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int unit = '0';
+	int tens = '0';
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (unit = 0; unit <= 9; unit++)
+		{
+			if (!((unit == tens) || (tens > unit)))
+			{
+				putchar(tens);
+				putchar(unit);
+				if (!(unit == 9 && tens == 8))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}		
