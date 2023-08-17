7b3633de78e9809891d6835428718acf40616dda
Michael <oshomike35@gmail.com>
2023-08-16 19:43:43 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,8 @@
  */
 int print_sign(int n)
 {
-	if (n > 0) {
+	if (n > 0) 
+	{
 		_putchar('+');
 		return (1);
 	}
@@ -18,7 +19,7 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 }
 
