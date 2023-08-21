fdd0ac590bb665616a8fcb68be62b13a4d22eb0f
Michael <oshomike35@gmail.com>
2023-08-21 00:17:23 -0700

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ * Description: a program that finds and prints the largest prime factor of the number 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i < sqrt(n); i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
