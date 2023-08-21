4ef1d4a7b343c3224c1daff85a6215fc24ed60e9
Michael <oshomike35@gmail.com>
2023-08-21 00:25:05 -0700

    Prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 /**
  * main - Entry point
@@ -10,7 +9,7 @@ int main(void)
 {
 	unsigned long int i, n = 612852475143;
 
-	for (i = 3; i < sqrt(n); i = i + 2)
+	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
 			n = n / i;
