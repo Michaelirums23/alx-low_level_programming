5c63b69d4d762fcb5132c9454da4747788958a2d
michaelirums23 <oshomike35@gmail.com>
2023-09-18 08:17:20 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,32 +10,27 @@
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
+	if (argc != 2)
+	{
         printf("Error\n");
         return 1;
-    }
-
-    int bytes = atoi(argv[1]);
-
-    if (bytes < 0)
-    {
+	}
+	int bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
         printf("Error\n");
         return 2;
-    }
+	}
+	unsigned char *main_ptr = (unsigned char *)main;
+	int i;
 
-    unsigned char *main_ptr = (unsigned char *)main;
-    int i;
-
-    for (i = 0; i < bytes; i++)
-    {
+	for (i = 0; i < bytes; i++)
+	{
         printf("%02x", main_ptr[i]);
-
-        if (i == bytes - 1)
+	if (i == bytes - 1)
             printf("\n");
         else
             printf(" ");
-    }
-
-    return 0;
+	}
+	return 0;
 }
